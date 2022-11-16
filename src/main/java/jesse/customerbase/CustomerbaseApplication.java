56e7a35fd049f6bc621414b85bbfecd285809027
jesreu <jesse.reunamo@gmail.com>
2022-11-16 03:16:58 +0200

    updating commandline

--- src/main/java/jesse/customerbase/CustomerbaseApplication.java
+++ src/main/java/jesse/customerbase/CustomerbaseApplication.java
@@ -20,7 +20,7 @@ public class CustomerbaseApplication {
 	}
 
 	@Bean
-	public CommandLineRunner CmdRunner(CustomerRepository crepository,GroupRepository grepository, UserRepository urepository) {
+	public CommandLineRunner CmdRunner(CustomerRepository crepository,GroupRepository grepository) {
 		return (args) -> {
 			//tallennetaan hieman testidataa
 			//lisää ryhmän kanta-asiakas
@@ -28,14 +28,6 @@ public class CustomerbaseApplication {
 			grepository.save(group1);
 			//lisää yhden asiakkaan
 			crepository.save(new Customer("Matti", "Meikäläinen", 22, "matti.meikalainen@gmail.com", "+358020202", group1));
-			
-			// Lisätään pari käyttäjää
-			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6",
-					"user@kayttaja.test", "USER");
-			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C",
-					"admin@kayttaja.test", "ADMIN");
-			urepository.save(user1);
-			urepository.save(user2);
 		};
 
 	}
